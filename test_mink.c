b991037636d8d97aedd2a91c794c06714e82283f
Chloe Arluck <chloearluck@gmail.com>
2017-07-20 08:38:41 -0700

    Remove old code from test_mink

--- test_mink.c
+++ test_mink.c
@@ -67,44 +67,11 @@ void saveWithShells(Polyhedron * poly, char * filename) {
   }
 }
 
-void test_cells(Polyhedron * p, char * s) {
-  p->formCells();
-  cout<<s<<" has "<<p->cells.size()<<" cells"<<endl;
-  for (int i=1; i<p->cells.size(); i++) {
-    cout<<"cell "<<i<<endl;
-    Cell * c = p->cells[i];
-    bool valid = !c->getShell(0)->getHFaces()[0]->pos();
-    if (valid) {
-      for (int j=0; j<c->nShells(); j++) 
-        cout<<j<<": "<<( c->getShell(j)->outer() ? "outer" : "inner")<<endl;
-      PTR<Point> q = pointInCell(p, i);
-      cout<<"p: "<<q->getP().getX().mid()<<" "<<q->getP().getY().mid()<<" "<<q->getP().getZ().mid()<<endl;
-    }
-  }
-  saveWithShells(p, s);
-}
-
 int main (int argc, char *argv[]) {
   if (argc == 2) { 
     unsigned seed = atoi(argv[1]);
     srandom(seed);
   }
-
-  // double b1[6] = { 1, 2, 1, 2, 1, 2};
-  // Polyhedron * box1 = box(b1);
-
-  // double b2[6] = { -1, -2, -1, -2, -1, -2};
-  // Polyhedron * box2 = box(b2);
-
-  // double b3[6] = { -3, 3, -3, 3, -3, 3};
-  // Polyhedron * box3 = box(b3);
-
-  // Polyhedron * disjoint = box1->boolean(box2, Union);
-  // Polyhedron * nested = complement(box3, box1);
-
-  // test_cells(box3, "box3");
-  // test_cells(nested, "nested");
-  // test_cells(disjoint, "disjoint");
 
   Polyhedron * robot = loadPoly("rect-drone.vtk");
   Polyhedron * room = loadPoly("room.vtk");
