f24bb49243ca1f20db1cb3dc95335a7ea9e06ed7
Chloe Arluck <chloearluck@gmail.com>
2017-09-28 14:26:27 -0400

    New Transformation classes

--- path3d.cc
+++ path3d.cc
@@ -1,5 +1,96 @@
 #include "path3d.h"
 
+class TransformationData {
+ public:
+  PV3 u, v, w, t;
+  TransformationData () {}
+  TransformationData (PV3 u, PV3 v, PV3 w, PV3 t) : u(u), v(v), w(w), t(t) {}
+  Parameter & operator[](int i) { 
+    if (i < 3) return u[i];
+    if (i < 6) return v[i%3];
+    if (i < 9) return w[i%3];
+    return t[i%3];
+  }
+  int size () const { return 12; }
+};
+
+class Transformation : public Object<TransformationData> {
+ public:
+  PV3 getU () { return get().u; }
+  PV3 getV () { return get().v; }
+  PV3 getW () { return get().w; }
+  PV3 getT () { return get().t; }
+};
+
+//transformation which rotates d about bc to lie in the same plane as abc
+class UnfoldTriangleTransformation : public Transformation {
+  PTR<Point> pa, pb, pc, pd;
+  TransformationData calculate () {
+    PV3 a = pa->getP();
+    PV3 b = pb->getP();
+    PV3 c = pc->getP();
+    PV3 d = pd->getP();
+
+    PV3 u = (c-b).unit();
+    PV3 uprime = u;
+    PV3 w = (d-b).cross(u).unit();
+    PV3 wprime = u.cross(a-b).unit();
+    PV3 v = w.cross(u);
+    PV3 vprime = wprime.cross(u);
+
+    PV3 t = b - b.dot(u)*(uprime) - b.dot(v)*(vprime) - b.dot(w)*(wprime);
+    PV3 x = uprime.getX()*u + vprime.getX()*v + wprime.getX()*w;
+    PV3 y = uprime.getY()*u + vprime.getY()*v + wprime.getY()*w;
+    PV3 z = uprime.getZ()*u + vprime.getZ()*v + wprime.getZ()*w;
+    return TransformationData(x,y,z,t);
+  }
+ public:
+  UnfoldTriangleTransformation(PTR<Point> pa, PTR<Point> pb, PTR<Point> pc, PTR<Point> pd) : pa(pa), pb(pb), pc(pc), pd(pd) {}
+};
+
+class CompositeTransformation : public Transformation {
+  PTR<Transformation> f, g;
+  TransformationData calculate () {
+    PV3 u1 = g->getU();
+    PV3 v1 = g->getV();
+    PV3 w1 = g->getW();
+    PV3 t1 = g->getT();
+    PV3 u2 = f->getU();
+    PV3 v2 = f->getV();
+    PV3 w2 = f->getW();
+    PV3 t2 = f->getT();
+
+    PV3 x1 = PV3(u1.getX(), v1.getX(), w1.getX());
+    PV3 y1 = PV3(u1.getY(), v1.getY(), w1.getY());
+    PV3 z1 = PV3(u1.getZ(), v1.getZ(), w1.getZ());
+
+    PV3 u = PV3(x1.dot(u2), y1.dot(u2), z1.dot(u2)); 
+    PV3 v = PV3(x1.dot(v2), y1.dot(v2), z1.dot(v2)); 
+    PV3 w = PV3(x1.dot(w2), y1.dot(w2), z1.dot(w2)); 
+    PV3 t = PV3(u2.dot(t1), v2.dot(t1), w2.dot(t1)) + t2;
+
+    return TransformationData(u,v,w,t);
+  }
+ public:
+  CompositeTransformation(PTR<Transformation> f, PTR<Transformation> g) : f(f), g(g) {}
+};
+
+//apply transformation t to point p
+class TransformedPoint : public Point {
+  PTR<Point> point;
+  PTR<Transformation> t;
+  PV3 calculate () {
+    PV3 p = point->getP();
+    PV3 u = t->getU();
+    PV3 v = t->getV();
+    PV3 w = t->getW();
+    PV3 tt = t->getT();
+    return PV3(u.dot(p), v.dot(p), w.dot(p)) + tt;
+  }
+ public:
+  TransformedPoint(PTR<Point> point, PTR<Transformation> t) : point(point), t(t) {}
+};
+
 struct ComparePointOrder {
   PTR<Point> r;
   ComparePointOrder(PTR<Point> r) : r(r) {}
