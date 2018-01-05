2d5883ab7dc4d5e0831255a8df61f4dfed369335
Chloe Arluck <chloearluck@gmail.com>
2018-01-05 08:11:30 -0500

    latest hull (untested)

--- hull.C
+++ hull.C
@@ -1,220 +1,5 @@
 #include "hull.h"
 
-bool HullEdge::horizonEdge (HullVertex *v) const 
-{
-  return !face->conflict(v) && twin->face->conflict(v);
-}
-
-bool HullFace::visible (HullVertex *v) const
-{
-  HullEdge *f = boundary->twin->next, *g = f->twin->next;
-  int s = Orientation(boundary->tail->p, f->tail->p, g->tail->p, v->p);
-  return s == 1 || s == 0 && contains(v);
-}
-
-bool HullFace::contains (HullVertex *v) const
-{
-  Point *a = v->p;
-  Points b;
-  b.push_back(boundary->tail->p);
-  b.push_back(boundary->head()->p);
-  b.push_back(boundary->twin->next->head()->p);
-  TrianglePlane tp(b[0], b[1], b[2]);
-  int c = projectionCoordinate(&tp);
-  for (int i = 0; i < 3; ++i)
-    if (LeftTurn(a, b[i], b[(i+1)%3], c) < 1)
-      return false;
-  return true;
-}
-
-bool HullFace::conflict (HullVertex *v) const
-{
-  return find(conflicts.begin(), conflicts.end(), v) != conflicts.end();
-}
-
-Hull::~Hull ()
-{
-  for (HullVertices::iterator v = vertices.begin(); v != vertices.end(); ++v)
-    delete *v;
-  for (HullEdges::iterator e = edges.begin(); e != edges.end(); ++e)
-    delete *e;
-  for (HullFaces::iterator f = faces.begin(); f != faces.end(); ++f)
-    delete *f;
-}
-
-HullVertex * Hull::addVertex (Point *p)
-{
-  HullVertex *v = new HullVertex(p);
-  vertices.push_back(v);
-  return v;
-}
-
-HullEdge * Hull::addEdge (HullVertex *t, HullVertex *h)
-{
-  HullEdge *e = new HullEdge(t, 0, 0), *et = new HullEdge(h, e, 0);
-  e->twin = et;
-  edges.push_back(e);
-  edges.push_back(et);
-  return e;
-}
-
-HullFace * Hull::addFace (HullEdge *e)
-{
-  HullEdge *f = e->twin->next, *g = f->twin->next;
-  HullFace *face = new HullFace(e);
-  faces.push_back(face);
-  e->face = face;
-  f->face = face;
-  g->face = face;  
-  return face;
-}
-
-void Hull::formHull ()
-{
-  initHull();
-  for (int i = 4; i < vertices.size(); ++i)
-    expandHull(i);
-}
-
-void Hull::initHull ()
-{
-  HullEdge *e[12];
-  e[0] = addEdge(vertices[0], vertices[1]);
-  e[3] = e[0]->twin;
-  e[1] = addEdge(vertices[0], vertices[2]);
-  e[6] = e[1]->twin;
-  e[2] = addEdge(vertices[0], vertices[3]);
-  e[9] = e[2]->twin;
-  e[4] = addEdge(vertices[1], vertices[2]);
-  e[7] = e[4]->twin;
-  e[5] = addEdge(vertices[1], vertices[3]);
-  e[10] = e[5]->twin;
-  e[8] = addEdge(vertices[2], vertices[3]);
-  e[11] = e[8]->twin;
-  for (int i = 0; i < 4; ++i) {
-    vertices[i]->edge = e[3*i];
-    if (Orientation(e[3*i]->head()->p, e[3*i+1]->head()->p, e[3*i+2]->head()->p, e[3*i]->tail->p) == -1)
-      for (int j = 0; j < 3; ++j)
-        e[3*i+j]->next = e[3*i+(j+1)%3];
-    else
-      for (int j = 0; j < 3; ++j)
-	      e[3*i+(j+1)%3]->next = e[3*i+j];
-  }
-  for (int i = 0; i < 12; ++i)
-    if (!e[i]->face)
-      addFace(e[i]);
-  for (int i = 4; i < vertices.size(); ++i)
-    for (int j = 0; j < 4; ++j)
-      if (faces[j]->visible(vertices[i])) {
-	faces[j]->conflicts.push_back(vertices[i]);
-	vertices[i]->conflicts.push_back(faces[j]);
-      }
-}
-
-void Hull::expandHull (int i)
-{
-  HullVertex *v = vertices[i];
-  if (v->conflicts.empty())
-    return;
-  HullEdges h, ev;
-  findHorizon(v, h);
-  addCone(v, h);
-  removeVisible(i);
-}
-
-void Hull::findHorizon (HullVertex *v, HullEdges &h) const
-{
-  HullEdge *e = findHorizonEdge(v), *e0 = e;
-  do {
-    h.push_back(e);
-    e = e->twin;
-    while (!e->horizonEdge(v))
-      e = e->next;
-  }
-  while (e != e0);
-}
-
-HullEdge * Hull::findHorizonEdge (HullVertex *v) const
-{
-  for (HullFaces::iterator f = v->conflicts.begin();
-       f != v->conflicts.end(); ++f) {
-    HullEdge *e = (*f)->boundary;
-    for (int i = 0; i < 3; ++i, e = e->twin->next)
-      if (e->twin->horizonEdge(v))
-	return e->twin;
-  }
-  return 0;
-}
-
-void Hull::addCone (HullVertex *v, HullEdges &h)
-{
-  HullEdge *ep = *(h.end()-1), *evp = 0;
-  for (HullEdges::iterator e = h.begin(); e != h.end(); ++e) {
-    HullEdge *ev = addEdge(v, (*e)->tail);
-    if (evp)
-      evp->next = ev;
-    else
-      v->edge = ev;
-    ev->twin->next = ep->twin;
-    (*e)->next = ev->twin;
-    (*e)->tail->edge = *e;
-    evp = ev;
-    ep = *e;
-  }
-  evp->next = v->edge;
-  for (HullEdges::iterator e = h.begin(); e != h.end(); ++e)
-    addConeFace(v, *e);
-}
-
-void Hull::addConeFace (HullVertex *v, HullEdge *e)
-{
-  const HullVertices &ec = e->face->conflicts, &etc = e->twin->face->conflicts;
-  HullFace *f = addFace(e->twin);
-  for (HullVertices::const_iterator w = ec.begin(); w != ec.end(); ++w)
-    if (*w != v && f->visible(*w)) {
-      f->conflicts.push_back(*w);
-      (*w)->conflicts.push_back(f);
-    }
-  for (HullVertices::const_iterator w = etc.begin(); w != etc.end(); ++w)
-    if (*w != v &&
-	find(f->conflicts.begin(), f->conflicts.end(), *w) == f->conflicts.end()
-	&& f->visible(*w)) {
-      f->conflicts.push_back(*w);
-      (*w)->conflicts.push_back(f);
-    }
-}
-
-void Hull::removeVisible (int i) const
-{
-  const HullFaces &c = vertices[i]->conflicts;
-  for (int j = i + 1; j < vertices.size(); ++j) {
-    HullVertex *v = vertices[j];
-    for (HullFaces::const_iterator f = c.begin(); f != c.end(); ++f) {
-      HullFaces::iterator g = remove(v->conflicts.begin(), v->conflicts.end(), *f);
-      v->conflicts.erase(g, v->conflicts.end());
-    }
-  }
-  for (HullFaces::const_iterator f = c.begin(); f != c.end(); ++f)
-    (*f)->boundary = 0;
-}
-
-Polyhedron * Hull::polyhedron () const
-{
-  Polyhedron *a = new Polyhedron;
-  map<HullVertex *, Vertex *> vmap;
-  for (HullVertices::const_iterator v = vertices.begin(); v != vertices.end(); ++v)
-    vmap.insert(pair<HullVertex *, Vertex *>(*v, a->getVertex((*v)->p)));
-  for (HullFaces::const_iterator f = faces.begin(); f != faces.end(); ++f) {
-    HullEdge *e = (*f)->boundary;
-    if (e) {
-      HullVertex *u = e->tail, *v = e->head(), *w = e->twin->next->head();
-      a->addTriangle(vmap.find(u)->second, vmap.find(v)->second,
-		     vmap.find(w)->second);
-    }
-  }
-  return a;
-}
-
 Polyhedron * convexHull (Polyhedron *a)
 {
   Points pts;
@@ -223,36 +8,26 @@ Polyhedron * convexHull (Polyhedron *a)
   return convexHull(pts);
 }
 
-Polyhedron * convexHull (const Points &pts)
+Polyhedron * convexHull (Points &pts)
 {
-  int n = pts.size(), *p = new int [n], i = 2;
-  randomPermutation(n, p);
-  while (pts[p[0]]->onLine(pts[p[1]], pts[p[i]]))
-    ++i;
-  if (i > 2) {
-    int t = p[2];
-    p[2] = p[i];
-    p[i] = t;
-  }
-  i = 3;
-  while (Orientation(pts[p[0]], pts[p[1]], pts[p[2]], pts[p[i]]) == 0)
-    ++i;
-  if (i > 3) {
-    int t = p[3];
-    p[3] = p[i];
-    p[i] = t;
-  }
-  Hull hull;
-  for (int i = 0; i < n; ++i) {
-    hull.addVertex(pts[p[i]]);
-  }
+  int n = pts.size(), *p = randomPermutation(n);
+  Points ppts;
+  for (int i = 0; i < n; ++i)
+    ppts.push_back(pts[p[i]]);
   delete [] p;
-  hull.formHull();
-  return hull.polyhedron();
+  Polyhedron *a = hullInit(ppts);
+  if (!a)
+    return 0;
+  ConflictGraph cg = conflictGraphInit(a);
+  for (Vertices::iterator v = a->vertices.begin() + 4; v != a->vertices.end(); ++v)
+    expandHull(a, *v, cg);
+  a->removeNullFaces();
+  return a;
 }
 
-void randomPermutation (int n, int *p)
+int * randomPermutation (int n)
 {
+  int *p = new int [n];
   for (int i = 0; i < n; ++i)
     p[i] = i;
   for (int i = 0; i < n - 1; ++i) {
@@ -260,7 +35,8 @@ void randomPermutation (int n, int *p)
     p[i] = p[j];
     p[j] = ti;
   }
-}    
+  return p;
+}
 
 int randomInteger (int lb, int ub)
 {
@@ -269,43 +45,173 @@ int randomInteger (int lb, int ub)
   return lb + d;
 }
 
-/*int main (int argc, char *argv[])
+Polyhedron * hullInit (Points &pts)
+{
+  int n = pts.size();
+  if (n < 4)
+    return 0;
+  int i = 2;
+  while (i < n && pts[i]->onLine(pts[0], pts[1]))
+    ++i;
+  if (i == n)
+    return 0;
+  if (i > 2) {
+    PTR<Point> p = pts[2];
+    pts[2] = pts[i];
+    pts[i] = p;
+  }
+  i = 3;
+  int s;
+  while (i < n) {
+    s = Orientation(pts[0], pts[1], pts[2], pts[i]);
+    if (s)
+      break;
+    ++i;
+  }
+  if (i == n)
+    return 0;
+  if (i > 3) {
+    PTR<Point> p = pts[3];
+    pts[3] = pts[i];
+    pts[i] = p;
+  }
+  if (s == -1)
+    reverse(pts.begin(), pts.begin() + 2);
+  Polyhedron *a = new Polyhedron;
+  for (Points::iterator p = pts.begin(); p != pts.end(); ++p)
+    a->getVertex(*p);
+  a->addTriangle(a->vertices[0], a->vertices[1], a->vertices[3]);
+  a->addTriangle(a->vertices[1], a->vertices[2], a->vertices[3]);
+  a->addTriangle(a->vertices[2], a->vertices[0], a->vertices[3]);
+  a->addTriangle(a->vertices[2], a->vertices[1], a->vertices[0]);
+  return a;
+}
+
+ConflictGraph::~ConflictGraph ()
+{
+  for (map<Vertex *, FaceSet *>::iterator i = vcon.begin(); i != vcon.end(); ++i)
+    delete i->second;
+  for (map<Face *, VertexSet *>::iterator i = fcon.begin(); i != fcon.end(); ++i)
+    delete i->second;
+}
+
+void ConflictGraph::insert (Vertex *v, Face *f)
+{
+  map<Vertex *, FaceSet *>::iterator vi = vcon.find(v);
+  if (vi == vcon.end()) {
+    FaceSet *fs = new FaceSet;
+    fs->insert(f);
+    vcon.insert(pair<Vertex *, FaceSet *>(v, fs));
+  }
+  else
+    vi->second->insert(f);
+  map<Face *, VertexSet *>::iterator fi = fcon.find(f);
+  if (fi == fcon.end()) {
+    VertexSet *vs = new VertexSet;
+    vs->insert(v);
+    fcon.insert(pair<Face *, VertexSet *>(f, vs));
+  }
+  else
+    fi->second->insert(v);
+}
+
+void ConflictGraph::erase (Face *f)
+{
+  VertexSet *vs = fcon.find(f)->second;
+  for (VertexSet::iterator v = vs->begin(); v != vs->end(); ++v) 
+    vcon.find(*v)->second->erase(f);
+}
+
+FaceSet * ConflictGraph::conflicts (Vertex *v)
+{
+  map<Vertex *, FaceSet *>::iterator i = vcon.find(v);
+  return i == vcon.end() || i->second->empty() ? 0 : i->second;
+}
+
+VertexSet * ConflictGraph::conflicts (Face *f)
+{
+  map<Face *, VertexSet *>::iterator i = fcon.find(f);
+  return i == fcon.end() || i->second->empty() ? 0 : i->second;
+}
+
+void ConflictGraph::update (Vertex *v, Face *f, VertexSet *vs)
+{
+  if (vs)
+    for (VertexSet::iterator w = vs->begin(); w != vs->end(); ++w)
+      if (*w != v && visible(*w, f))
+	insert(*w, f);
+}
+
+ConflictGraph conflictGraphInit (Polyhedron *a)
+{
+  ConflictGraph cg;
+  for (Vertices::iterator v = a->vertices.begin() + 4; v != a->vertices.end(); ++v)
+    for (Faces::iterator f = a->faces.begin(); f != a->faces.end(); ++f)
+      if (visible(*v, *f))
+	cg.insert(*v, *f);
+  return cg;
+}
+
+bool visible (Vertex *v, Face *f)
+{
+  return v->getP()->side(f->getP()) == 1;
+}
+
+void expandHull (Polyhedron *a, Vertex *v, ConflictGraph &cg)
+{
+  FaceSet *fs = cg.conflicts(v);
+  if (!fs)
+    return;
+  HEdges he = horizon(*fs);
+  for (HEdges::iterator h = he.begin(); h != he.end(); ++h)
+    expandHull(a, v, cg, *h);
+  Faces fa(fs->begin(), fs->end());
+  for (Faces::iterator f = fa.begin(); f != fa.end(); ++f) {
+    cg.erase(*f);
+    a->removeLoop((*f)->getBoundary(0));
+  }
+}
+
+HEdges horizon (const FaceSet &fs)
+{
+  HEdges he;
+  for (FaceSet::const_iterator f = fs.begin(); f != fs.end(); ++f) {
+    HEdges fe;
+    (*f)->boundaryHEdges(fe);
+    for (HEdges::iterator g = fe.begin(); g != fe.end(); ++g)
+      if (fs.find((*g)->ccw()->getF()) == fs.end())
+	he.push_back(*g);
+  }
+  return he;
+}
+
+void expandHull (Polyhedron *a, Vertex *v, ConflictGraph &cg, HEdge *h)
+{
+  VertexSet *vs1 = cg.conflicts(h->getF()), *vs2 = cg.conflicts(h->ccw()->getF());
+  Face *f = a->addTriangle(v, h->tail(), h->head());
+  cg.update(v, f, vs1);
+  cg.update(v, f, vs2);  
+}
+
+/*
+#include "io.h"
+
+int main (int argc, char *argv[])
 {
   if (argc < 2)
     return 0;
   ifstream astr(argv[1]);
   if (!astr.good())
     return 0;
+  inputPerturbed = argc == 2 ? true : *argv[2] != 't';
   Parameter::enable();
-  Polyhedron *a = readPolyhedronVTK(astr),
+  Polyhedron *a = readPolyhedronVTK(astr, inputPerturbed),
     *b = convexHull(a);
+  b->formCells();
+  b->describe();
+  writePolyhedronVTK(b->faces, cout);
   delete b;
   delete a;
   Parameter::disable();
-}*/
-
-// debug
-
-void pp (Point *p);
-
-void pv (HullVertex *v)
-{
-  pp(v->p);
 }
-
-void pe (HullEdge *e)
-{
-  cerr << "(";
-  pv(e->tail);
-  pv(e->head());
-  cerr << ")" << endl;
-}
-
-void pf (HullFace *f)
-{
-  cerr << "(";
-  pv(f->boundary->tail);
-  pv(f->boundary->twin->next->tail);
-  pv(f->boundary->twin->next->twin->next->tail);
-  cerr << ")" << endl;
-}
+*/
--- hull.h
+++ hull.h
@@ -1,77 +1,38 @@
 #ifndef HULL
 #define HULL
 
-#include "io.h"
-
-class HullEdge;
-
-class HullFace;
-
-class HullVertex {
- public:
-  HullVertex (Point *p) : p(p), edge(0) {}
-  PTR<Point> p;
-  HullEdge *edge;
-  vector<HullFace *> conflicts;
-};
-
-typedef vector<HullVertex *> HullVertices;
-
-class HullEdge {
- public:
-  HullEdge (HullVertex *tail, HullEdge *twin, HullEdge *next) 
-    : tail(tail), twin(twin), next(next), face(0), flag(false) {}
-  HullVertex * head () const { return twin->tail; }
-  bool horizonEdge (HullVertex *v) const;
-
-  HullVertex *tail;
-  HullEdge *twin, *next;
-  HullFace *face;
-  bool flag;
-};
-
-typedef vector<HullEdge *> HullEdges;
-
-class HullFace {
- public:
-  HullFace (HullEdge *boundary) : boundary(boundary) {}
-  bool visible (HullVertex *v) const;
-  bool contains (HullVertex *v) const;
-  bool conflict (HullVertex *v) const;
-
-  HullEdge *boundary;
-  HullVertices conflicts;
-};
-
-typedef vector<HullFace *> HullFaces;
-
-class Hull {
- public:
-  ~Hull ();
-  HullVertex * addVertex (Point *p);
-  HullEdge * addEdge (HullVertex *t, HullVertex *h);
-  HullFace * addFace (HullEdge *e);
-  void formHull ();
-  void initHull ();
-  void expandHull (int i);
-  void findHorizon (HullVertex *v, HullEdges &h) const;
-  HullEdge * findHorizonEdge (HullVertex *v) const;
-  void addCone (HullVertex *v, HullEdges &h);
-  void addConeFace (HullVertex *v, HullEdge *e);
-  void removeVisible (int i) const;
-  Polyhedron * polyhedron () const;
-
-  HullVertices vertices;
-  HullEdges edges;
-  HullFaces faces;
-};
+#include "polyhedron.h"
 
 Polyhedron * convexHull (Polyhedron *a);
 
-Polyhedron * convexHull (const Points &pts);
+Polyhedron * convexHull (Points &pts);
 
-void randomPermutation (int n, int *p);
+int * randomPermutation (int n);
 
 int randomInteger (int lb, int ub);
+
+Polyhedron * hullInit (Points &pts);
+
+class ConflictGraph {
+  map<Vertex *, FaceSet *> vcon;
+  map<Face *, VertexSet *> fcon;
+ public:
+  ~ConflictGraph ();
+  void insert (Vertex *v, Face *f);
+  void erase (Face *f);
+  FaceSet * conflicts (Vertex *v);
+  VertexSet * conflicts (Face *f);
+  void update (Vertex *v, Face *f, VertexSet *vs);
+};
+
+ConflictGraph conflictGraphInit (Polyhedron *a);
+
+bool visible (Vertex *v, Face *f);
+
+void expandHull (Polyhedron *a, Vertex *v, ConflictGraph &cg);
+
+HEdges horizon (const FaceSet &fs);
+
+void expandHull (Polyhedron *a, Vertex *v, ConflictGraph &cg, HEdge *h);
 
 #endif
