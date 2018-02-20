f954250fcc81226453caa0a13b0afbae53eb0b72
Chloe Arluck <chloearluck@gmail.com>
2018-02-20 08:15:23 -0500

    Update create_test_example.cc to create elongated cube example polyhedron

--- create_test_example.cc
+++ create_test_example.cc
@@ -2,6 +2,7 @@
 #include "io.h"
 #include <cstring>
 #include "simplify.h"
+#include "hull.h"
 
 Polyhedron * loadPoly(const char * filename) {
   Polyhedron * poly;
@@ -28,32 +29,60 @@ void savePoly(Polyhedron * p, const char * filename) {
   }
 }
 
+Primitive2(DiffZ, PTR<Point>, i, PTR<Point>, j);
+int DiffZ::sign() { return (i->getP().getZ() - j->getP().getZ()).sign(); }
+struct CompareZ {
+  bool operator()(PTR<Point> i, PTR<Point> j) {
+    return (DiffZ(i, j) < 0); 
+  }
+};
+
 int main (int argc, char *argv[]) {
   Parameter::enable();
 
-  double d1[6] = {-5, 5, -5, 5, -5, 5};
-  Polyhedron * big_box = box(d1);
+  PTR<Point> top1 = new InputPoint(-1,-1, 1); //top left
+  PTR<Point> top2 = new InputPoint(-1, 1, 1); //top right
+  PTR<Point> top3 = new InputPoint( 1, 1, 1); //bottom right
+  PTR<Point> top4 = new InputPoint( 1,-1, 1); //bottom left
 
-  double d2[6] = {1, 4, -4, 4, -4, 4};
-  Polyhedron * room1 = box(d2);
-  
-  double d3[6] = {-4, -1, -4, 4, -4, 4};
-  Polyhedron * room2 = box(d3);
+  PTR<Point> bottom1 = new InputPoint(-1,-1,-1); //top left
+  PTR<Point> bottom2 = new InputPoint(-1, 1,-1); //top right
+  PTR<Point> bottom3 = new InputPoint( 1, 1,-1); //bottom right
+  PTR<Point> bottom4 = new InputPoint( 1,-1,-1); //bottom left
 
-  double d4[6] = {-2, 2, -.75, .75, -1.5, 0};
-  Polyhedron * hall1 = box(d4);
+  //find the midpoint of the edges you want to split
+  PTR<Point> mid1 = new MidPoint(top1, top4);
+  PTR<Point> mid2 = new MidPoint(top1, top2);
+  //add a (0,0,.000001) to them
+  PTR<Point> p1 = new SumPoint(mid1, new InputPoint(0,0,0.0000001));
+  PTR<Point> p2 = new SumPoint(mid2, new InputPoint(0,0,0.0000001));
 
-  double d5[6] = {-3.25, -1.75, 3, 6, 1, 2.5};
-  Polyhedron * hall2 = box(d5);
+  Points plist;
+  plist.push_back(top1);
+  plist.push_back(top3);
+  plist.push_back(top4);
+  plist.push_back(p1);
+  plist.push_back(bottom1);
+  plist.push_back(bottom2);
+  plist.push_back(bottom3);
+  plist.push_back(bottom4);
+  Polyhedron * hull1 = convexHull(plist, true);
+  assert(hull1 != NULL);
 
-  Polyhedron * out = big_box;
-  out = out->boolean(room1, Complement);
-  out = out->boolean(room2, Complement);
-  out = out->boolean(hall1, Complement);
-  out = out->boolean(hall2, Complement);
-  // savePoly(out, "tworooms.vtk");
-  simplify(out, 1e-6);
-  savePoly(out, "tworooms.vtk");
+  plist.clear();
+  plist.push_back(top1);
+  plist.push_back(top2);
+  plist.push_back(top3);
+  plist.push_back(p2);
+  plist.push_back(bottom1);
+  plist.push_back(bottom2);
+  plist.push_back(bottom3);
+  plist.push_back(bottom4);
+  Polyhedron * hull2 = convexHull(plist, true);
+  assert(hull2 != NULL);
 
+  Polyhedron * cube = hull1->boolean(hull2, Union);
+
+  savePoly(cube, "split-faces-cube.vtk");
 
 }
